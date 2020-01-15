e08578e06a940e15fb26be06c67283f36d76fd26
YeseulK <yeseullk@gmail.com>
2020-01-15 09:31:49 +0900

    Update member domain

--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -39,18 +39,21 @@ public class MemberServiceTest {
         assertEquals(member, memberRepository.findOne(savedId));
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void 중복_회원_예외() throws Exception {
         //given
         Member member1 = new Member();
-        member1.setName("kim1");
+        member1.setName("kim");
 
         Member member2 = new Member();
-        member2.setName("kim1");
+        member2.setName("kim");
 
         //when
+        memberService.join(member1);
+        memberService.join(member2); //예외 발생
 
         //then
+        fail("예외가 발생해야 한다.");
     }
 
 }
