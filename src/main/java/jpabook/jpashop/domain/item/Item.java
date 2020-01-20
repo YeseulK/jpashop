5ed3fed2bf1b677562ccad79e5a50bb690ccb50b
YeseulK <yeseullk@gmail.com>
2020-01-20 17:07:48 +0900

    Optimize performance 컬렉션 조회 최적화
    V1~V3

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import jpabook.jpashop.domain.Category;
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
